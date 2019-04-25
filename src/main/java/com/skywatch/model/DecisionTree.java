8117a82f62c5fb929126f14b12abaae6520fc75a
Muhammad Naufal <nau930@gmail.com>
2019-04-25 19:20:54 +0700

    created decision tree skeleton

---
+++ src/main/java/com/skywatch/model/DecisionTree.java
@@ -0,0 +1,39 @@
+package com.skywatch.model;
+
+import com.skywatch.model.Crash;
+import com.skywatch.algorithms.InformationGain;
+import java.util.ArrayList;
+
+
+class Node {
+    Crash crashKey;
+    boolean boolKey;
+    Node left, right;
+
+    public Node(boolean bool) {
+        crashKey = null;
+        boolKey = bool;
+        left = right = null;
+    }
+
+    public Node(Crash crashObject){
+        crashKey = crashObject;
+        boolKey = false;
+        left = right = null;
+
+    }
+    
+}
+
+class DecisionTree {
+    Node root;
+
+    DecisionTree(Crash crashObject) {
+        root = new Node(crashObject);
+    }
+
+    DecisionTree(boolean bool) {
+        root = new Node(bool);
+    }
+    
+}
