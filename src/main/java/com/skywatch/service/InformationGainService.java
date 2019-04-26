30f973d0c56a9724a6fd52b109e03e7eb27a097d
Muhammad Naufal <nau930@gmail.com>
2019-04-26 10:22:06 +0700

    create find child gain, temporary commit for merge

--- src/main/java/com/skywatch/service/InformationGainService.java
+++ src/main/java/com/skywatch/service/InformationGainService.java
@@ -12,7 +12,7 @@ import java.util.List;
 
 @Service
 public class InformationGainService {
-
+`
     private final CrashRepository crashRepository;
 
     private final InformationGainDataRepository informationGainDataRepository;
@@ -95,7 +95,43 @@ public class InformationGainService {
         return booleanEntropyImpurity(numerator, numOfTrue + numOfFalse) - findRemainder(numOfTrue, numOfFalse, attribute);
     }
 
+    private double findChildGain(int numOfTrue, int numOfFalse, String attributeParent, boolean bool) {
+        int numerator = calculateNumOfCrashedTrueChild(attribute, bool);
+        return booleanEntropyImpurity(numerator, informationGainDataRepository.findByRatingIs(bool)) - findRemainder(numOfTrue, numOfFalse, attribute);
+    }
+
     public InformationGainData calculateGain() {
+        double[] data = new double[5];
+        data[0] = findGain(
+                crashRepository.findByRatingIs(true).size(),
+                crashRepository.findByRatingIs(false).size(),
+                "rating"
+        );
+        data[1] = findGain(
+                crashRepository.findByModelAgeIs(true).size(),
+                crashRepository.findByModelAgeIs(false).size(),
+                "modelAge"
+        );
+        data[2] = findGain(
+                crashRepository.findByFirstFlightIs(true).size(),
+                crashRepository.findByFirstFlightIs(false).size(),
+                "firstFlight"
+        );
+        data[3] = findGain(
+                crashRepository.findByPilotAgeIs(true).size(),
+                crashRepository.findByPilotAgeIs(false).size(),
+                "pilotAge"
+        );
+        data[4] = findGain(
+                crashRepository.findByWeatherIs(true).size(),
+                crashRepository.findByWeatherIs(false).size(),
+                "weather"
+        );
+        informationGainDataRepository.deleteAll();
+        return informationGainDataRepository.save(new InformationGainData(data));
+    }
+
+    public InformationGainData calculateChildGain() {
         double[] data = new double[5];
         data[0] = findGain(
                 crashRepository.findByRatingIs(true).size(),
