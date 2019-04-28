2bbe2f3b993bec1b7a7f40642404b2b47a2ac8e1
Nardiena Althafia Pratama <nardienapratama@yahoo.com>
2019-04-28 11:40:07 +0700

    made remainder and half of getinformation gain method

--- src/main/java/com/skywatch/service/NewInformationGainService.java
+++ src/main/java/com/skywatch/service/NewInformationGainService.java
@@ -18,4 +18,29 @@ public class NewInformationGainService {
         return -(probNumOfTrue * (Math.log(probNumOfTrue) / Math.log(2)) +
                 (1 - probNumOfTrue) * (Math.log(1 - probNumOfTrue) / Math.log(2)));
     }
+
+    private double remainder(int trueCount, int falseCount) {
+        return (double) (trueCount) / (trueCount + falseCount) * entropy(trueCount, falseCount) +
+                (double) (falseCount) / (trueCount + falseCount) * entropy(trueCount, falseCount);
+    }
+
+    public double getInformationGain(boolean root, String attribute, String parent, boolean parentBool) {
+        if (root) {
+            switch (attribute) {
+                case "rating":
+                    return entropy(crashRepository.findByCrashedIs(true).size(), crashRepository.findByCrashedIs(false).size()) -
+                            remainder(crashRepository.findByRatingIs(true).size(), crashRepository.findByRatingIs(false).size());
+                case "modelAge":
+                    return entropy(crashRepository.findByCrashedIs(true).size(), crashRepository.findByCrashedIs(false).size()) -
+                            remainder(crashRepository.findByModelAgeIs(true).size(), crashRepository.findByModelAgeIs(false).size());
+                case "firstFlight":
+                    return entropy(crashRepository.findByCrashedIs(true).size(), crashRepository.findByCrashedIs(false).size()) -
+                            remainder(crashRepository.findByFirstFlightIs(true).size(), crashRepository.findByFirstFlightIs(false).size());
+                case "pilotAge":
+                    return entropy(crashRepository.findByCrashedIs(true).size(), crashRepository.findByCrashedIs(false).size()) -
+                            remainder(crashRepository.findByPilotAgeIs(true).size(), crashRepository.findByPilotAgeIs(false).size());
+            }
+        }
+    }
+
 }
