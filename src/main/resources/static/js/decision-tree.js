d73f7ce6ba12f130ad7a30ebbd4f7444dd24c5c3
Nardiena Althafia Pratama <nardienapratama@yahoo.com>
2019-05-04 18:09:49 +0700

    tried to make fucntion in js file and changed location of submit button div

--- src/main/resources/static/js/decision-tree.js
+++ src/main/resources/static/js/decision-tree.js
@@ -1,14 +1,21 @@
 $(document).ready(function () {
     console.log('ready');
     $('#submit-button').click(function () {
-        $.ajax({
-            url: "predict?rating=" + $(".ratingRadio:checked").val() +
-            "&modelAge=" + $(".modelAgeRadio:checked").val() + 
+        console.log("it works");
+        let urlVar = "predict?rating=" + $(".ratingRadio:checked").val() +
+            "&modelAge=" + $(".modelAgeRadio:checked").val() +
             "&firstFlight=" + $(".firstFlightRadio:checked").val() +
             "&pilotAge=" + $(".pilotAgeRadio:checked").val() +
-            "&weather=" + $(".weatherRadio:checked").val(),
+            "&weather=" + $(".weatherRadio:checked").val();
+        // console.log(url);
+        $.ajax({
+            url: urlVar,
             type: "GET",
             success: function (result) {
+                let finalresult = result['crashed'];
+                if (finalresult == true) {
+
+                }
                 console.log(result['crashed'])
             }
         })
