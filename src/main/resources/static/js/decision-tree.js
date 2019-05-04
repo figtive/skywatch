e05e8ba28993047c09972c793089eb07f4b09978
Nardiena Althafia Pratama <nardienapratama@yahoo.com>
2019-05-04 15:45:46 +0700

    changed form layout and fixed js

--- src/main/resources/static/js/decision-tree.js
+++ src/main/resources/static/js/decision-tree.js
@@ -2,7 +2,7 @@ $(document).ready(function () {
     console.log('ready');
     $('.predict').click(function () {
         $.ajax({
-            url: "predict?rating=" + true + "&modelAge=" + true + "&firstFlight=" + true + "&pilotAge=" + false + "&weather=" + false + "&crashed=" + false,
+            url: "predict?rating=" + $("#rating").val() + "&modelAge=" + true + "&firstFlight=" + true + "&pilotAge=" + false + "&weather=" + false + "&crashed=" + false,
             type: "GET",
             success: function (result) {
                 console.log(result['crashed'])
