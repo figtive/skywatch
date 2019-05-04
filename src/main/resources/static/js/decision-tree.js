a8582607d856d0fa1b1eea785977dc4bd8815cca
Nardiena Althafia Pratama <nardienapratama@yahoo.com>
2019-05-04 16:27:23 +0700

    fixed button, edited inor changes in js

--- src/main/resources/static/js/decision-tree.js
+++ src/main/resources/static/js/decision-tree.js
@@ -1,16 +1,16 @@
 $(document).ready(function () {
     console.log('ready');
-    $('.predict').click(function () {
+    $('#submit-button').click(function () {
         $.ajax({
-            url: "predict?rating=" + $(".ratingRadio:checked").val() + 
+            url: "predict?rating=" + $(".ratingRadio:checked").val() +
             "&modelAge=" + $(".modelAgeRadio:checked").val() + 
             "&firstFlight=" + $(".firstFlightRadio:checked").val() +
             "&pilotAge=" + $(".pilotAgeRadio:checked").val() +
-            "&weather=" + $(".weathergRadio:checked").val(),
+            "&weather=" + $(".weatherRadio:checked").val(),
             type: "GET",
             success: function (result) {
                 console.log(result['crashed'])
-            },
+            }
         })
     })
 });
