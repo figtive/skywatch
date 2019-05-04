06b1bb7edf838fb7fd488c3c6c686ba15daa77a9
Muhammad Naufal <nau930@gmail.com>
2019-05-04 15:53:48 +0700

    fixed decision tree get js method, grammar correction on radio buttons

--- src/main/resources/static/js/decision-tree.js
+++ src/main/resources/static/js/decision-tree.js
@@ -2,7 +2,11 @@ $(document).ready(function () {
     console.log('ready');
     $('.predict').click(function () {
         $.ajax({
-            url: "predict?rating=" + $("#rating").val() + "&modelAge=" + true + "&firstFlight=" + true + "&pilotAge=" + false + "&weather=" + false + "&crashed=" + false,
+            url: "predict?rating=" + $(".ratingRadio:checked").val() + 
+            "&modelAge=" + $(".modelAgeRadio:checked").val() + 
+            "&firstFlight=" + $(".firstFlightRadio:checked").val() +
+            "&pilotAge=" + $(".pilotAgeRadio:checked").val() +
+            "&weather=" + $(".weathergRadio:checked").val(),
             type: "GET",
             success: function (result) {
                 console.log(result['crashed'])
